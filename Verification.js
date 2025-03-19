0d1e375b7ac47193297d209057411fe8e920a139
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-03-19 22:14:38 +0530

    Email UC2

--- Verification.js
+++ Verification.js
@@ -1,43 +1,43 @@
-// //PIN CODE UC 1
-// const pinCodePattern = /^.{6}$/; 
-// const pinCode = "4A0088"; 
+//PIN CODE UC 1
+const pinCodePattern = /^.{6}$/; 
+const pinCode = "4A0088"; 
 
-// if (pinCodePattern.test(pinCode)) {
-//     console.log("Valid PIN Code");
-// } else {
-//     console.log("Invalid PIN Code");
-// }
+if (pinCodePattern.test(pinCode)) {
+    console.log("Valid PIN Code");
+} else {
+    console.log("Invalid PIN Code");
+}
 
 
-// //PIN CODE UC2
-// const pinCodePattern = /^[1-9][0-9]{5}$/;
-// const pinCode = "A400088";
+//PIN CODE UC2
+const pinCodePattern = /^[1-9][0-9]{5}$/;
+const pinCode = "A400088";
 
-// if (pinCodePattern.test(pinCode)) {
-//     console.log("Valid PIN Code");
-// } else {
-//     console.log("Invalid PIN Code");
-// }
+if (pinCodePattern.test(pinCode)) {
+    console.log("Valid PIN Code");
+} else {
+    console.log("Invalid PIN Code");
+}
 
-// //PIN CODE UC3
-// const pinCodePattern = /^[0-9]{5}[0-9]$/; // Ensures exactly 6 digits
-// const pinCode = "400088B"; // Test case
+//PIN CODE UC3
+const pinCodePattern = /^[0-9]{5}[0-9]$/; // Ensures exactly 6 digits
+const pinCode = "400088B"; // Test case
 
-// if (pinCodePattern.test(pinCode)) {
-//     console.log("Valid PIN Code");
-// } else {
-//     console.log("Invalid PIN Code");
-// }
+if (pinCodePattern.test(pinCode)) {
+    console.log("Valid PIN Code");
+} else {
+    console.log("Invalid PIN Code");
+}
 
-// //PIN CODE UC4 
-// const pinCodePattern = /^[0-9]{3}\s?[0-9]{3}$/; // Allows optional space
-// const pinCode = "400 088"; // Test case
+//PIN CODE UC4 
+const pinCodePattern = /^[0-9]{3}\s?[0-9]{3}$/; // Allows optional space
+const pinCode = "400 088"; // Test case
 
-// if (pinCodePattern.test(pinCode)) {
-//     console.log("Valid PIN Code");
-// } else {
-//     console.log("Invalid PIN Code");
-// }
+if (pinCodePattern.test(pinCode)) {
+    console.log("Valid PIN Code");
+} else {
+    console.log("Invalid PIN Code");
+}
 
 //Email UC1
 using System;
@@ -57,3 +57,17 @@ class EmailValidator
         Console.WriteLine(ValidateEmail(email) ? "Valid Email" : "Invalid Email");
     }
 }
+
+//Email UC2
+const emailRegex = /^abc@bridgelabz$/;
+
+const testEmails = [
+    "abc@bridgelabz",   // Valid
+    "abc@xyz",          // Invalid
+    "xyz@bridgelabz",   // Invalid
+    "abc.bridgelabz"    // Invalid (missing @)
+];
+
+testEmails.forEach(email => {
+    console.log(`${email}: ${emailRegex.test(email)}`);
+});
