9be281d98fbd0672a47460f77a66281e485aedff
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-03-19 22:38:32 +0530

    UC1 Ability to create a Address Book Contact

---
+++ AddressBook.js
@@ -0,0 +1,21 @@
+//UC1
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    display() {
+        return `Name: ${this.firstName} ${this.lastName}, Address: ${this.address}, ${this.city}, ${this.state} - ${this.zip}, Phone: ${this.phoneNumber}, Email: ${this.email}`;
+    }
+}
+
+// Example Usage
+let contact1 = new Contact("John", "Doe", "123 Main St", "New York", "NY", "10001", "9876543210", "john.doe@example.com");
+console.log(contact1.display());
