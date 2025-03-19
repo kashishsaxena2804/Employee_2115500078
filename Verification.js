33fe620ef1f3c1f5f24e72b8fea61c0320cf99f6
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-03-19 21:39:17 +0530

    PinCode UC2

--- Verification.js
+++ Verification.js
@@ -1,6 +1,17 @@
 //UC 1
+const pinCodePattern = /^.{6}$/; 
+const pinCode = "4A0088"; 
+
+if (pinCodePattern.test(pinCode)) {
+    console.log("Valid PIN Code");
+} else {
+    console.log("Invalid PIN Code");
+}
+
+
+//UC2
 const pinCodePattern = /^[1-9][0-9]{5}$/;
-const pinCode = "400088";
+const pinCode = "A400088";
 
 if (pinCodePattern.test(pinCode)) {
     console.log("Valid PIN Code");
